c2466bc1138a92dbc102247b335b3aa6667207a3
Jason Hatfield <gitlab.h8fg@smiplelogin.com>
2023-07-06 11:26:58 -0400

    Initial Commit

---
+++ CST-350/Topic-2/ButtonGrid/Models/ButtonModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ButtonGrid.Models
+{
+    public class ButtonModel
+    {
+        public int Id { get; set; }
+        public int ButtonState { get; set; }
+
+        public ButtonModel() { }
+
+        public ButtonModel(int id, int buttonState)
+        {
+            Id = id;
+            ButtonState = buttonState;
+        }
+    }
+}
---
+++ CST-350/Topic-2/ButtonGrid/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ButtonGrid.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
